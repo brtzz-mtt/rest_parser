edcc88a55aedc12cb070116724bd7cd692d954d4
brtzz-mtt <brtzz.mtt@gmail.com>
2021-11-18 11:57:36 +0100

    xlsx autodetect parsing

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import csv, json, os, requests, sys
 
 from datetime import date
 from pathlib import Path
-#from pprint import pformat, pprint
+from pprint import pformat, pprint
 
 from tools import clean_label
 
@@ -39,18 +39,28 @@ if __name__ == '__main__':
                 source_type = 'csv'
             elif 'json' in source[1]:
                 source_type = 'json'
+            else:
+                source_type = '???'
             print(f"Parsing {source_type} source '{source_label}'..",
                 end = ' ',
                 flush = True
             )
             response = requests.get(source_address)
             if response.status_code == 200:
+                file_type = 't'
                 if source_type == 'csv':
                     data = list(csv.reader(response.text))
+                    dump = response.text
                 elif source_type == 'json':
                     data = json.loads(response.text)
-                dump = response.text
-                with open(DATA_PATH + date.today().strftime('%Y-%m-%d') + '_' + clean_label(source_label) + '.' + source_type, 'w+') as data_file:
+                    dump = response.text
+                else: # autodetecting source type..
+                    if 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' in response.headers['Content-Type']:
+                        source_type = 'xlsx'
+                        data = None # TBD https://www.marsja.se/your-guide-to-reading-excel-xlsx-files-in-python/
+                        dump = response.content
+                        file_type = 'b'
+                with open(DATA_PATH + date.today().strftime('%Y-%m-%d') + '_' + clean_label(source_label) + '.' + source_type, 'w' + file_type) as data_file:
                     data_file.write(dump)
                 print("OK!")
             else:
